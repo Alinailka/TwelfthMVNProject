91625a2790f1fbf70f65f26b6971ec9a13e066dd
Alinailka <alina-il@mail.ru>
2022-04-20 00:04:00 +0700

    In work

--- src/main/java/ru/netology/Game.java
+++ src/main/java/ru/netology/Game.java
@@ -1,89 +1,33 @@
 package ru.netology;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 public class Game {
-
-    Player[] players = new Player[0];
-
-    public void register(Player player) {
-        int length = players.length + 1;
-        Player[] tmp = new Player[length];
-        System.arraycopy(players, 0, tmp, 0, players.length);
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = player; //положить в ячейку предмет с принятым id
-        players = tmp;
-    }
-    //метод регистрации игрока; если игрок не зарегистрирован,
-    // то он не сможет играть в турнире.
-    // Вам нужно хранить всех зарегистрированных игроков в поле класса Game в виде списка.
-
-//
-
-    public boolean matches(Product product, String search) {
-        if (product.getName().contains(search)) {
-            return true;
-        } else {
-            return false;
-        }
-        // или в одну строку:
-        // return product.getName().contains(search);
+    public Collection<Player> players = new ArrayList<>();
+    public boolean registered(Player player) {
+        return players.add(player);
     }
 
-
-
-
-
-
-    public Player[] round(String playerName1, String playerName2) {
-        Player[] participants = new Player[2];
+    public int round(String playerName1, String playerName2) {
         for (Player player : players) {
-            if (player.getName() == playerName1) {
-
-//                int length = participants.length;
-//                Player[] tmp = new Player[length];
-                System.arraycopy(players, 0, participants, 0, participants.length);
-//                int lastIndex = participants.length - 1;
-//                tmp[lastIndex] = player;
-            //    participants = tmp;
+            if (contains(player, playerName1)) {
+                if (contains(player, playerName2)) {
+                    int result = player.compareTo(player);
+                    return result;
+                }
+                if (!contains(player, playerName2)) {
+                    throw new NotRegisteredException(playerName2);
+                }
             }
-            if (player.getName() != playerName1) {
-                throw new NotRegisteredException(playerName1);
+            if (!contains(player, playerName1)) {
+                throw new NotRegisteredException(playerName2);
             }
+        }
+    return 0;}
 
-          if (player.getName() == playerName2) {
-  System.arraycopy(players, 0, participants, 1, participants.length);}
-            if (player.getName() != playerName2) {
-                throw new NotRegisteredException(playerName1);
-            }
-
-//                int length = participants.length + 1;
-//                Player[] tmp = new Player[length];
-//                System.arraycopy(participants, 0, tmp, 0, participants.length);
-//                int lastIndex = tmp.length - 1;
-//                tmp[lastIndex] = player;
-//                participants = tmp;
-//                if (player.getName() != playerName2) {
-//                    throw new NotRegisteredException(playerName1);
-//                }
-//            }
-//        }
-
-     //   Arrays.sort(participants);
-        //todo сравнение у кого больше сила
-
-
+    public boolean contains(Player player, String search) {
+        return player.getName().contains(search);
     }
-        return participants;}
-    //метод соревнования между двумя игроками.
-    // Если хотя бы один из игроков незарегистрирован,
-    // должно выкинуться исключение NotRegisteredException
-    // (сами создайте его класс). Обратите внимание,
-    // что игроки передаются методу через имя -
-    // полную информацию об игроке нужно будет найти в коллекции зарегистрированных игроков
-    // (см. аналог в виде findById из прошлых дз).
-    // Выигрывает тот игрок, у которого больше strength (сила);
-    // если сила одинаковая, то будет ничья.
-    // Метод должен возвращать одно число - 0 в случае ничьи,
-    // 1 в случае победы первого игрока и 2 в случае победы второго игрока.
 }
--- src/test/java/ru/netology/GameTest.java
+++ src/test/java/ru/netology/GameTest.java
@@ -10,51 +10,69 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameTest {
 
     Game game = new Game();
-    Player [] players = new Player[0];
+    Player[] players = new Player[0];
 
-    Player first = new Player (1, "Petya",1 );
-    Player second = new Player (2, "Vasya",2 );
-    Player third = new Player (3, "Vanya",3 );
-    Player fourth = new Player (4, "Stepa",4 );
-    Player fifth = new Player (5, "Alesha",5 );
-
-
-
+    Player first = new Player(1, "Petya", 1);
+    Player second = new Player(2, "Vasya", 2);
+    Player third = new Player(3, "Vanya", 3);
+    Player fourth = new Player(4, "Stepa", 4);
+    Player fifth = new Player(5, "Alesha", 5);
     @Test
-    void register() {
-        game.register(first);
-        game.register(second);
-        game.register(third);
-        game.register(fourth);
-        Player [] expected = new Player[]{first, second, third, fourth};
-        Player [] actual = game.players;
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void round() {
-        game.register(first);
-        game.register(second);
-        game.round("Petya", "Vasya");
-        Player [] expected = new Player[]{first, second};
-        Player [] actual = game.round("Petya", "Vasya");
+    public void add() {
+        game.registered(first);
+        game.registered(second);
+        game.registered(third);
+        game.registered(fourth);
+        Player[] expected = new Player[]{first, second, third, fourth};
+        Player[] actual = game.players.toArray(new Player[0]);
         assertArrayEquals(expected, actual);
     }
     @Test
-    void roundIfPlayerNoRegistered() {
-        game.register(fourth);
-        assertThrows(NotRegisteredException.class, () -> {
-            game.round("Stepa", "Alesha");
-        });
-    }
-    @Test
-    void sortByStrength() {
-        game.register(first);
-        game.register(second);
-        Player[] expected = new Player[]{second,first,};
-        Player[] actual = new Player[]{second,first};
+    public void round() {
+        game.registered(first);
+        game.registered(second);
+        game.registered(third);
+        game.registered(fourth);
 
-        Arrays.sort(actual);
-      assertArrayEquals(expected, actual);
+        int expected = 2;
+        int actual = game.round("Petya", "Vasya");
+        assertEquals(expected, actual);
     }
+//    @Test
+//    void register() {
+//        game.register(first);
+//        game.register(second);
+//        game.register(third);
+//        game.register(fourth);
+//        Player [] expected = new Player[]{first, second, third, fourth};
+//        Player [] actual = game.players;
+//        assertArrayEquals(expected, actual);
+//    }
+//
+//    @Test
+//    void round() {
+//        game.register(first);
+//        game.register(second);
+//        game.round("Petya", "Vasya");
+//        Player [] expected = new Player[]{first, second};
+//        Player [] actual = game.round("Petya", "Vasya");
+//        assertArrayEquals(expected, actual);
+//    }
+//    @Test
+//    void roundIfPlayerNoRegistered() {
+//        game.register(fourth);
+//        assertThrows(NotRegisteredException.class, () -> {
+//            game.round("Stepa", "Alesha");
+//        });
+//    }
+//    @Test
+//    void sortByStrength() {
+//        game.register(first);
+//        game.register(second);
+//        Player[] expected = new Player[]{second,first,};
+//        Player[] actual = new Player[]{second,first};
+//
+//        Arrays.sort(actual);
+//      assertArrayEquals(expected, actual);
+//    }
 }
